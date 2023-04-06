c3451b74895137cb5a7d601f933260c9a2e41caa
stormsrw <114696416+stormsrw@users.noreply.github.com>
2023-04-06 13:34:01 -0400

    4/6/2023

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,75 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main extends JFrame {
 
         final int windowWidth = 350;
         final int windowHeight = 350;
-
+        private int currentCard =1;
+        JFrame window = new JFrame();
+        private  CardLayout card;
+        private JPanel cardPanel;
+        private JPanel loginPanel;
+        private JPanel displayPanel;
+        private JLabel loginLabel;
+        private JLabel dispalyLabel;
+        private JButton loginButton;
         public Main(){
             //JFrame window = new JFrame();
-            setTitle("booking");
+            setTitle("main");
             setSize(windowWidth,windowHeight);
+
+
+            //cardPanel
+            cardPanel = new JPanel();
+            card = new CardLayout();
+            cardPanel.setLayout(card);
+            //make login panel
+            loginPanel = new JPanel();
+            //make display panel
+            displayPanel = new JPanel();
+           //make login label
+            loginLabel = new JLabel("login panel");
+            //make display label
+            dispalyLabel= new JLabel("display pabel");
+
+            //add labels to appropriate panel
+            loginPanel.add(loginLabel);
+            loginPanel.add(loginButton);
+            displayPanel.add(dispalyLabel);
+            //add panels to cardpanel
+            cardPanel.add(loginLabel,1);
+            cardPanel.add(displayPanel,2);
+
+            loginButton.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent arg0)
+                {
+
+                    // used first c1 CardLayout
+                    card.first(cardPanel);
+
+                    // value of currentcard is 1
+                    currentCard = 2;
+                }
+            });
+
+
+
+            //displayGui x = new displayGui();
+            getContentPane().add(cardPanel, BorderLayout.CENTER);
+           // getContentPane().add(displayPanel,BorderLayout.CENTER);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             setVisible(true);
+
         }
 
-    public static void main(String[] args) {
-            Main gui = new Main();
 
-        displayGui x = new displayGui();
-        loginPage lp= new loginPage();
+    public static void main(String[] args) {
+            //Main gui = new Main();
+        Main cl = new Main();
         System.out.println("this is the right repository");
     }
 }
--- src/loginPage.java
+++ src/loginPage.java
@@ -1,17 +1,38 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class loginPage extends JFrame {
 
     final int windowWidth = 350;
     final int windowHeight = 350;
+    private JPanel loginPanel;
+    private JButton loginButton;
 
      public loginPage(){
          //JFrame window = new JFrame();
          setTitle("login page");
          setSize(windowWidth,windowHeight);
          setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         buildPanel();
+         add(loginPanel);
          setVisible(true);
      }
+
+    private void buildPanel() {
+         loginPanel = new JPanel();
+         loginButton = new JButton("login");
+         loginButton.addActionListener(new loginListener());
+         loginPanel.add(loginButton);
+
+    }
+    private class loginListener implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("this is the login button");
+
+        }
+    }
     public static void main(String[] args) {
          loginPage lp= new loginPage();
         System.out.println("this is the login gui");
